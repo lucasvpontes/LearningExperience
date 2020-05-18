c839da327a8a41d4f7ffece5ba21b78d9b2f9c33
lucasvpontes <lucas.ti.pontes@outlook.com>
2020-05-18 00:52:42 -0300

    Adicionando mongodb heroku

--- LearningExperience.Models/Interfaces/IMongoDbSettings.cs
+++ LearningExperience.Models/Interfaces/IMongoDbSettings.cs
@@ -9,5 +9,9 @@ namespace LearningExperience.Models
     {
         string DatabaseName { get; set; }
         string ConnectionString { get; set; }
+        string Host { get; set; }
+        int Port { get; set; }
+        string User { get; set; }
+        string Password { get; set; }
     }
 }
--- LearningExperience.Models/Model/MongoDbSettings.cs
+++ LearningExperience.Models/Model/MongoDbSettings.cs
@@ -4,5 +4,9 @@
     {
         public string DatabaseName { get; set; }
         public string ConnectionString { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
     }
 }
