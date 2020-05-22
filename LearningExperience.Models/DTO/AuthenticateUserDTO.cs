ae908997c31c64ff9ef0b6162b3891a9e29fd438
lucasvpontes <lucas.ti.pontes@outlook.com>
2020-05-22 01:37:34 -0300

    Subindo AuthenticateUserDTO

---
+++ LearningExperience.Models/DTO/AuthenticateUserDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LearningExperience.Models.DTO
+{
+    public class AuthenticateUserDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
