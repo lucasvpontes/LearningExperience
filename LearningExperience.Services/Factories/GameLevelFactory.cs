b20ac6bb9b097d39f520fd5b74a55e28b12f4968
Lucas Pontes <lucas.ti.pontes@outlook.com>
2020-09-11 21:07:44 -0300

    Fixing bug GameLevelFactory

--- LearningExperience.Services/Factories/GameLevelFactory.cs
+++ LearningExperience.Services/Factories/GameLevelFactory.cs
@@ -1,13 +1,9 @@
-﻿using LearningExperience.Services.Factories.GameLevelGenerators;
+﻿using LearningExperience.Models.DTO;
 using System;
 
 namespace LearningExperience.Services.Factories
 {
     public class GameLevelFactory
     {
-        public void ClientCode(GameLevelGenerator levelGenerator)
-        {
-            Console.WriteLine("That's just a test.\n" + levelGenerator.GenerateLevelLogic());
-        }
     }
 }
