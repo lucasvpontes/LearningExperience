223be385a8fd99779352fc70f6660011ac77701d
Lucas Pontes <lucas.ti.pontes@outlook.com>
2020-10-12 15:59:50 -0300

    returning to internal http to implement https on nginx

--- LearningExperience.Api/Program.cs
+++ LearningExperience.Api/Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -8,13 +7,15 @@ namespace LearningExperience.Api
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseSetting("https_port", "5001")
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
 
 
     }
