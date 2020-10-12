3b566c3c69a87849e672b0cc548b36a54f064b01
Lucas Pontes <lucas.ti.pontes@outlook.com>
2020-10-12 01:47:40 -0300

    Adding https

--- LearningExperience.Api/Program.cs
+++ LearningExperience.Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -7,15 +8,14 @@ namespace LearningExperience.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseSetting("https_port", "5001")
+                .UseStartup<Startup>();
+
 
     }
 }
