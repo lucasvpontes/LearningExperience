8cf076da68d8f862c7b239f92a3f9ba8cacd28c7
Lucas Pontes <lucas.ti.pontes@outlook.com>
2020-10-19 02:07:27 -0300

    Adding 6 level logic

---
+++ LearningExperience.Models/Model/ConcreteLevels/EqualFiveShapesLevel.cs
@@ -0,0 +1,34 @@
+﻿using LearningExperience.Models.DTO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearningExperience.Models.Model.ConcreteLevels
+{
+    public class EqualFiveShapesLevel: GameLevel
+    {
+        public override GameLevelResult Configure(IList<GameLevelImage> gameLevelImages)
+        {
+            IList<GameLevelImage> shuffledList = Shuffle(gameLevelImages).ToList();
+
+            var comparebleMain = shuffledList.First();
+            shuffledList.First().Match = true;
+            var exernalId = shuffledList.First().ExternalId;
+            var comparable = shuffledList.Single(s => s.Id == exernalId);
+            comparable.Match = true;
+
+            shuffledList = Shuffle(shuffledList).Where(s => s.Id != comparebleMain.Id &&
+                                                       s.Id != comparable.Id)
+                                                .Take(4).ToList();
+            shuffledList.Add(comparable);
+
+            GameLevelResult gameLevelResult = new GameLevelResult()
+            {
+                MainImage = comparebleMain,
+                Comparable = shuffledList
+            };
+
+            gameLevelResult.Comparable = Shuffle(gameLevelResult.Comparable);
+            return gameLevelResult;
+        }
+    }
+}
